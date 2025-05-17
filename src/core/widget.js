e043daab253524fee27ab4ff0ade0215c2a31c20
Simon Heppner <simon@heppner.at>
2025-05-18 00:17:40 +0200

    Google Maps Link on activity title

--- src/core/widget.js
+++ src/core/widget.js
@@ -1182,7 +1182,12 @@ export default class DianaWidget {
 
       this.elements.activityTimeBox.innerHTML = `
         <div class="activity-time-card">
-          <div class="activity-time-header">${this.config.activityName}</div>
+          <div class="activity-time-header">
+            <a style="display: inline-flex;" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/dir/?api=1&destination=${this.config.activityStartLocation}">
+              ${this.config.activityName}
+              <svg style="margin: 0 0 5px 4px;" width="12px" height="12px" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" mirror-in-rtl="true" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="var(--bg-apply-hover)" d="M12.1.6a.944.944 0 0 0 .2 1.04l1.352 1.353L10.28 6.37a.956.956 0 0 0 1.35 1.35l3.382-3.38 1.352 1.352a.944.944 0 0 0 1.04.2.958.958 0 0 0 .596-.875V.96a.964.964 0 0 0-.96-.96h-4.057a.958.958 0 0 0-.883.6z"></path> <path fill="var(--bg-apply-hover)" d="M14 11v5a2.006 2.006 0 0 1-2 2H2a2.006 2.006 0 0 1-2-2V6a2.006 2.006 0 0 1 2-2h5a1 1 0 0 1 0 2H2v10h10v-5a1 1 0 0 1 2 0z"></path> </g></svg>
+            </a>
+          </div>
           <div class="activity-time-meta">
             ${isMultiDayDisplay ? dateRangeDisplayHtml : ''}
             <div class="activity-time-row">
